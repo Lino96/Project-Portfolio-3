e85fb9a4b9029b903fb0dd33483b64b02e2e124e
Lino96 <procolo.falanga96@gmail.com>
2022-09-22 21:44:29 +0000

    Creating Board

--- run.py
+++ run.py
@@ -21,3 +21,19 @@ dict = {
     'L': 12,
 }
 
+print("\n-- STEP1: Creating Board --\n")
+w = int(input("Enter board width (max 12): "))+1
+w = int(input("Enter board height (max 12): "))+1
+letterLeftBorder = 1
+letterRightBorder = w-1
+numberUpBorder = 1
+numberDownBorder = h-1
+
+while( w > 13 or w < 1 or h > 13 or h< 1):
+    print("\nMax Height and Width are 12, please retry...")
+    w = int(input("Enter board width (max 12): "))+1
+    w = int(input("Enter board height (max 12): "))+1
+    letterLeftBorder = 1
+    letterRightBorder = w-1
+    numberUpBorder = 1
+    numberDownBorder = h-1
