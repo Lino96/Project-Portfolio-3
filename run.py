4aab8c06e073bc45b908e333a2985712d7a110e7
Lino96 <procolo.falanga96@gmail.com>
2022-09-22 21:38:17 +0000

    Inserting Dictionary for the board

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+#include <stdio.h>
+import random
+import string
+
+gamestate = 1
+print("Welcome to Battleship Game!\n")
+
+dict = {
+    'A': 1,
+    'B': 2,
+    'C': 3,
+    'D': 4,
+    'E': 5,
+    'F': 6,
+    'G': 7,
+    'H': 8,
+    'I': 9,
+    'J': 10,
+    'K': 11,
+    'L': 12,
+}
+
